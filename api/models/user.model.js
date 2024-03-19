6b4931757cbf57c0d29ead59d6028bc95d0fe39b
Abdelrhman Zeid <th3zeid@hotmail.com>
2024-03-19 06:28:25 +0200

    commit

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -68,6 +68,10 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    certificate: {
+      type: String,
+      required: false,
+    },
   },
   {
     timestamps: true,
